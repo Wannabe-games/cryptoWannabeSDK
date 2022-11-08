0f17ba00cb5938bae0cc2e802868e5a371b7a7f0
Michał Fereniec <michalkoks99@gmail.com>
2022-11-08 14:01:00 +0100

    Initial commit

---
+++ iOS SDK/iOS_SDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_SDKApp.swift
+//  iOS SDK
+//
+//  Created by Michał Fereniec on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS_SDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
