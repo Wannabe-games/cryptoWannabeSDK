621f8e1161ab2398473dd0e9af85803af1bd3d2a
Michał Fereniec <michalkoks99@gmail.com>
2022-11-09 18:28:29 +0100

    Created "EXAMPLES" scheme in order to let our user better understand how to use the pod

---
+++ iOS-SDK/iOS-SDK EXAMPLES/iOS_SDK_EXAMPLESApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_SDK_EXAMPLESApp.swift
+//  iOS-SDK EXAMPLES
+//
+//  Created by Michał Fereniec on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS_SDK_EXAMPLESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
