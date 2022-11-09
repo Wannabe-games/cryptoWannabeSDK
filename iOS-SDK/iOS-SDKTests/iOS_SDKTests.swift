c8ca7878015f908673b747cd316faf3dffcb4ac3
Michał Fereniec <michalkoks99@gmail.com>
2022-11-09 15:17:11 +0100

    Initial commit replaced w/ proper project setup (Sngle View App -> Framework)

--- iOS-SDK/iOS-SDKTests/iOS_SDKTests.swift
+++ iOS-SDK/iOS-SDKTests/iOS_SDKTests.swift
@@ -1,13 +1,14 @@
 //
 //  iOS_SDKTests.swift
-//  iOS SDKTests
+//  iOS-SDKTests
 //
-//  Created by Michał Fereniec on 08/11/2022.
+//  Created by Michał Fereniec on 09/11/2022.
 //
 
 import XCTest
+@testable import iOS_SDK
 
-final class iOS_SDKTests: XCTestCase {
+class iOS_SDKTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -27,7 +28,7 @@ final class iOS_SDKTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
