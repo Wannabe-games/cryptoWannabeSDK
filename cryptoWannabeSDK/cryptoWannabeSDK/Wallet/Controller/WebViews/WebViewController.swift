d30694b8d235cecb59d04596f6fbdbfb1171e49f
Michał Fereniec <michalkoks99@gmail.com>
2022-12-06 23:11:15 +0100

    added webView funcs during/after navigation

--- cryptoWannabeSDK/cryptoWannabeSDK/Wallet/Controller/WebViews/WebViewController.swift
+++ cryptoWannabeSDK/cryptoWannabeSDK/Wallet/Controller/WebViews/WebViewController.swift
@@ -24,24 +24,41 @@ public class WebViewController: UIViewController {
         guard let proxyURL = URL(string: "https://proxy.wannabe.games/connect/\(UserDefaultsHandler.token)") else { return }
         
         var request = URLRequest(url: proxyURL)
-//        request.setValue("8lTWSnJCHxA0NG1aOjwUL0j0vtGjA7HmqDQP900UrVCpKJntwd", forHTTPHeaderField: "auth")
+        //        request.setValue("8lTWSnJCHxA0NG1aOjwUL0j0vtGjA7HmqDQP900UrVCpKJntwd", forHTTPHeaderField: "auth")
         webView.load(request)
+        
+        self.webView.navigationDelegate = self
     }
 }
 
 extension WebViewController: WKNavigationDelegate {
-
+    
+    public func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("Michał: fromCommit \(webView.url?.absoluteString)")
+    }
+    
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-//        debugPrint("Michał \(String(describing: webView.url?.absoluteString))")
+        print("Michał: fromFinish \(webView.url?.absoluteString)")
+        
+        guard let absoluteString = webView.url?.absoluteString else { return }
+        if absoluteString.hasPrefix("https://proxy.wannabe.games/api/connect") && absoluteString.hasSuffix("wallet") {
+//            Tu coś się dzieje
+        }
+        
+        print("Michał: token \(UserDefaultsHandler.token)")
+        if absoluteString == "https://proxy.wannabe.games/connect/\(UserDefaultsHandler.token)" {
+//        if absoluteString == "https://proxy.wannabe.games/api/connect/\(UserDefaultsHandler.token)/wallet" {
+            //Tu najprawdopodobniej dostanę odpowiedz
+            
+            webView.evaluateJavaScript("document.body.innerHTML") { html, error in
+                print("Michał: evaluate2")
+                print(html)
+            }
+        }
     }
 
     public func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
         debugPrint("webViewWebContent Process Did Terminate")
-        self.webView.reload()
-    }
-
-    public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        debugPrint("did Fail Navigation")
-        self.reloadInputViews() // redundant ?
+//        self.webView.reload()
     }
 }
--- cryptoWannabeSDK/cryptoWannabeSDK/Wallet/Server/AsyncService.swift
+++ cryptoWannabeSDK/cryptoWannabeSDK/Wallet/Server/AsyncService.swift
@@ -5,9 +5,90 @@
 //  Created by Michał Fereniec on 05/12/2022.
 //
 
-import Foundation
-
-class AsyncService {
-    
-    
-}
+//import Foundation
+//
+//class AsyncService {
+//
+////                   DispatchQueue.main.async {
+////                       completion(.success(result))
+////
+////                   }
+////               } else {
+////                   completion(.failure(.decodingError))
+////               }
+//                   
+//    func redirectTo2ndURL () {
+//                       
+//    }
+//    
+//    func listeningToLoginEndopint () {
+//        
+//    }
+//    
+//    public func register(request: HTTPRequest, response: HTTPResponse) {
+//        do {
+//            let registerUserDto = try request.getObjectFromRequest(RegisterUserDto.self)
+//            let user = registerUserDto.toUser()
+//
+//            try self.usersService.add(entity: user)
+//
+//            guard let registeredUser = try self.usersService.get(byId: user.id) else {
+//                return response.sendNotFoundError()
+//            }
+//
+//            let registeredUserDto = UserDto(user: registeredUser)
+//            return response.sendJson(registeredUserDto)
+//        }
+//        catch let error where error is DecodingError || error is RequestError {
+//            response.sendBadRequestError()
+//        }
+//        catch let error as ValidationsError {
+//            response.sendValidationsError(error: error)
+//        }
+//        catch {
+//            response.sendInternalServerError(error: error)
+//        }
+//    }
+//    
+//    
+//    public func generateHash(salt: String) throws -> String {
+//        let stringWithSalt = salt + self
+//
+//        guard let stringArray = stringWithSalt.digest(.sha256)?.encode(.base64) else {
+//            throw GeneratePasswordError()
+//        }
+//
+//        guard let stringHash = String(data: Data(bytes: stringArray, count: stringArray.count), encoding: .utf8) else {
+//            throw GeneratePasswordError()
+//        }
+//
+//        return stringHash
+//    }
+//    
+//    public func signIn(request: HTTPRequest, response: HTTPResponse) {
+//        do {
+//            let signIn = try request.getObjectFromRequest(SignInDto.self)
+//
+//            guard let user = try self.usersService.get(byEmail: signIn.email) else {
+//                return response.sendNotFoundError()
+//            }
+//
+//            let password = try signIn.password.generateHash(salt: user.salt)
+//            if password != user.password {
+//                return response.sendNotFoundError()
+//            }
+//
+//            let tokenDto = try self.prepareToken(user: user)
+//            return response.sendJson(tokenDto)
+//        }
+//        catch let error where error is DecodingError || error is RequestError {
+//            response.sendBadRequestError()
+//        }
+//        catch let error as ValidationsError {
+//            response.sendValidationsError(error: error)
+//        }
+//        catch {
+//            response.sendInternalServerError(error: error)
+//        }
+//    }
+//}
