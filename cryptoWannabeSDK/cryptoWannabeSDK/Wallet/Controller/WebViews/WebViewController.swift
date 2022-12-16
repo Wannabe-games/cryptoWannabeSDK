f98afc89781098ac308af8448497a418835eb3da
Michał Fereniec <michalkoks99@gmail.com>
2022-12-16 10:00:42 +0100

    added .gitignore

--- cryptoWannabeSDK/cryptoWannabeSDK/Wallet/Controller/WebViews/WebViewController.swift
+++ cryptoWannabeSDK/cryptoWannabeSDK/Wallet/Controller/WebViews/WebViewController.swift
@@ -35,9 +35,9 @@ public class WebViewController: UIViewController, WKNavigationDelegate {
         webView.navigationDelegate = self
         webView.allowsBackForwardNavigationGestures = true
        // sniff here/
-        loadData(from: proxyURL) { NetworkResult in
+//        loadData(from: proxyURL) { NetworkResult in
             //
-        }
+//        }
         sniff()
        
     }
@@ -64,82 +64,82 @@ public class WebViewController: UIViewController, WKNavigationDelegate {
 
 extension WebViewController {
   
-    public func getLoginInfo(completion: @escaping ((String) -> Void) ) {
-        guard let proxyURL = URL(string: "https://proxy.wannabe.games/api/token") else { return }
-        var request = URLRequest(url: proxyURL)
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else { return }
-          //  guard let model = try? JSONDecoder().decode(LoginResponse.self, from: data) else { return }
-            //print("Michał login model: \(model)")
-        }
-    }
-    
-    func loadData(from url: URL,
-                     completionHandler: @escaping (NetworkResult) -> Void) {
-           let task = URLSession.shared.dataTask(with: url) { data, _, error in
-//let result = data.map(NetworkResult.success) ?? .failure(error)
-//completionHandler(result)
-           }
-           task.resume()
-       }
-
-    public func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-        print("Michał: fromCommit \(String(describing: webView.url?.absoluteString))")
-       // getLoginInfo()
-//        loadData(from: url) { <#NetworkResult#> in
-//            <#code#>
+//    public func getLoginInfo(completion: @escaping ((String) -> Void) ) {
+//        guard let proxyURL = URL(string: "https://proxy.wannabe.games/api/token") else { return }
+//        var request = URLRequest(url: proxyURL)
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            guard let data = data else { return }
+//          //  guard let model = try? JSONDecoder().decode(LoginResponse.self, from: data) else { return }
+//            //print("Michał login model: \(model)")
 //        }
-        var loginExample = LoginResponse(status: "success", responseInfo: "Data exist", method: "GET", data: "profile:...")
-        print(loginExample.status + " " + loginExample.responseInfo + " "  + loginExample.data + " " + loginExample.method + " " + loginExample.data)
-        guard let absoluteString = webView.url?.absoluteString else { return }
-        if absoluteString.hasPrefix("https://proxy.wannabe.games/api/connect") && absoluteString.hasSuffix(token) {
-//            Tu coś się dzieje
-//            UIApplication.shared.open(absoluteString, completionHandler: nil)
-        }
+//    }
+    
+//    func loadData(from url: URL,
+//                     completionHandler: @escaping (NetworkResult) -> Void) {
+//           let task = URLSession.shared.dataTask(with: url) { data, _, error in
+////let result = data.map(NetworkResult.success) ?? .failure(error)
+////completionHandler(result)
+//           }
+//           task.resume()
+//       }
+
+//    public func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+//        print("Michał: fromCommit \(String(describing: webView.url?.absoluteString))")
+//       // getLoginInfo()
+////        loadData(from: url) { <#NetworkResult#> in
+////            <#code#>
+////        }
+//        var loginExample = LoginResponse(status: "success", responseInfo: "Data exist", method: "GET", data: "profile:...")
+//        print(loginExample.status + " " + loginExample.responseInfo + " "  + loginExample.data + " " + loginExample.method + " " + loginExample.data)
+//        guard let absoluteString = webView.url?.absoluteString else { return }
+//        if absoluteString.hasPrefix("https://proxy.wannabe.games/api/connect") && absoluteString.hasSuffix(token) {
+////            Tu coś się dzieje
+////            UIApplication.shared.open(absoluteString, completionHandler: nil)
+//        }
     }
     //deeplink
     //universallink
-    //bridge
-    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("Michał: fromFinish \(String(describing: webView.url?.absoluteString))")
-        
-        guard let absoluteString = webView.url?.absoluteString else { return }
-        if absoluteString.hasPrefix("https://proxy.wannabe.games/api/connect") && absoluteString.hasSuffix("wallet") {
-//            Tu coś się dzieje
-//            UIApplication.shared.open(absoluteString, completionHandler: nil)
-
-        }
-        //otworzyc przez chrome, a nie safari z tym linkiem
-        print("Michał: token \(UserDefaultsHandler.token)")
-//        if absoluteString == "https://proxy.wannabe.games/connect/\(UserDefaultsHandler.token)" {
-        if absoluteString == "https://proxy.wannabe.games/api/connect/\(UserDefaultsHandler.token)/wallet" {
-            
-            //JSON with user data probably here
-            
-            webView.evaluateJavaScript("document.body.innerHTML") { html, error in
-                print("Michał: evaluate2")
-                print(html ?? nil)
-                //maybe decode html here to get json with data about user login
-            }
-            //lub
-            // 2 opcja
-            // Wyciągnąc ciastka i inne rzeczy które autoryzują https
-        }
-    }
-
-    public func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
-        debugPrint("webViewWebContent Process Did Terminate")
-        self.webView.reload()
-    }
-    
-//    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Swift.Void) {
+//    //bridge
+//    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        print("Michał: fromFinish \(String(describing: webView.url?.absoluteString))")
+//
+//        guard let absoluteString = webView.url?.absoluteString else { return }
+//        if absoluteString.hasPrefix("https://proxy.wannabe.games/api/connect") && absoluteString.hasSuffix("wallet") {
+////            Tu coś się dzieje
+////            UIApplication.shared.open(absoluteString, completionHandler: nil)
+//
+//        }
+//        //otworzyc przez chrome, a nie safari z tym linkiem
+//        print("Michał: token \(UserDefaultsHandler.token)")
+////        if absoluteString == "https://proxy.wannabe.games/connect/\(UserDefaultsHandler.token)" {
+//        if absoluteString == "https://proxy.wannabe.games/api/connect/\(UserDefaultsHandler.token)/wallet" {
+//
+//            //JSON with user data probably here
+//
+//            webView.evaluateJavaScript("document.body.innerHTML") { html, error in
+//                print("Michał: evaluate2")
+//                print(html ?? nil)
+//                //maybe decode html here to get json with data about user login
+//            }
+//            //lub
+//            // 2 opcja
+//            // Wyciągnąc ciastka i inne rzeczy które autoryzują https
+//        }
+//    }
+//
+//    public func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+//        debugPrint("webViewWebContent Process Did Terminate")
+//        self.webView.reload()
+//    }
+//
+////    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Swift.Void) {
+////        decisionHandler(.allow)
+////    }
+//    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+//        if navigationAction.navigationType == .linkActivated {
+//            guard let url = navigationAction.request.url else {return}
+//            webView.load(URLRequest(url: url))
+//        }
 //        decisionHandler(.allow)
 //    }
-    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        if navigationAction.navigationType == .linkActivated {
-            guard let url = navigationAction.request.url else {return}
-            webView.load(URLRequest(url: url))
-        }
-        decisionHandler(.allow)
-    }
-}
+//}
